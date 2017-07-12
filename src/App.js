dfcf1fec3f76eadd34f9b8312be32b64b20dabae
Haley Kalb <haley.kalb@gmail.com>
2017-07-12 12:05:19 -0600

    added header component

--- src/App.js
+++ src/App.js
@@ -27,18 +27,6 @@ const App = ({
     <div className="container">
       <div className="row">
         <Header />
-        // <nav>
-        //   <div className="nav-wrapper teal lighten-2">
-        //     <div className="col s12">
-        //       <a href="#" className="brand-logo">Example Component</a>
-        //       <ul id="nav-mobile" className="right hide-on-med-and-down">
-        //         <li><a href="#">React</a></li>
-        //         <li><a href="#">Redux</a></li>
-        //         <li><a href="#">React Router</a></li>
-        //       </ul>
-        //     </div>
-        //   </div>
-        // </nav>
       </div>
       <div className="row">
         <div className="col s12">
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,17 +3,18 @@ import React from 'react'
 const Header = ({
 }) => {
   return (
-    <header className="row">
-      <nav>
-        <div className="container">
-          <div className="nav-wrapper">
-            <div className="col s12">
-              <a href="/" className="brand-logo">Shopping Cart</a>
-            </div>
-          </div>
+    <nav>
+      <div className="nav-wrapper teal lighten-2">
+        <div className="col s12">
+          <a href="#" className="brand-logo">Example Component</a>
+          <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <li><a href="#">React</a></li>
+            <li><a href="#">Redux</a></li>
+            <li><a href="#">React Router</a></li>
+          </ul>
         </div>
-      </nav>
-    </header>
+      </div>
+    </nav>
   )
 }
 
