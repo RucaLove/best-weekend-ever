8c3f3622212e5fb3f5da7246e9e7bcc940b39552
Haley Kalb <haley.kalb@gmail.com>
2017-07-12 12:30:49 -0600

    added side nav

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import 'materialize-css/dist/css/materialize.min.css';
 import Header from './components/Header';
+import Nav from './components/Nav';
 
 // class App extends Component {
 //   render() {
@@ -27,6 +28,7 @@ const App = ({
     <div className="container">
       <div className="row">
         <Header />
+        <Nav />
       </div>
       <div className="row">
         <div className="col s12">
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,14 +4,9 @@ const Header = ({
 }) => {
   return (
     <nav>
-      <div className="nav-wrapper teal lighten-2">
+      <div className="nav-wrapper yellow lighten-2">
         <div className="col s12">
-          <a href="#" className="brand-logo">Example Component</a>
-          <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li><a href="#">React</a></li>
-            <li><a href="#">Redux</a></li>
-            <li><a href="#">React Router</a></li>
-          </ul>
+          <a href="#" className="brand-logo center">Gameify Yo Summah</a>
         </div>
       </div>
     </nav>
