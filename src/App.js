a85b4292735f956dcf7972c8f9582374b4b0c8ec
Adam Smith <asmith13194@gmail.com>
2017-07-13 12:04:43 -0600

    clean

--- src/App.js
+++ src/App.js
@@ -5,35 +5,6 @@ import Header from './components/Header';
 import Results from './components/Results';
 import Events from './components/Events';
 
-const events = [
-  {name:'Swim',
-  points:3,
-  img: "http://media.istockphoto.com/photos/friends-enjoying-a-pool-party-picture-id524323159?k=6&m=524323159&s=612x612&w=0&h=_IBOSTI2_sR_pkaspsp28LyaguoTCZ_-VUhvoGWbLQs="},
-  {name:'Run',
-  points:2,
-  img: "https://s-media-cache-ak0.pinimg.com/originals/9b/95/88/9b958800fc7f853ac84ff29800013c3d.jpg"},
-  {name:'Hike',
-  points:4,
-  img: "http://i52.tinypic.com/m9t9cl.jpg"},
-  {name:'Festival',
-  points:10,
-  img: "http://ampthemag.com/wp-content/uploads/2017/03/fest-girl.jpg"},
-  {name:'BBQ',
-  points:4,
-  img: "http://i4.mirror.co.uk/incoming/article3533182.ece/ALTERNATES/s615/Raging-fire-from-charcoal-barbecue-grill.jpg"},
-  {name: 'Rock Climbing',
-  points: 5,
-  img:"http://www.toptenz.net/wp-content/uploads/2013/05/Rock-Climbing.jpg"},
-  {name: 'Movie Theater',
-  points: 1,
-  img:"http://timothywhite.com/sites/default/files/images/portfolio/pee-wee_herman.jpg"},
-  {name: 'Party',
-  points: 5,
-  img:"http://now-here-this.timeout.com/wp-content/uploads/2013/12/House-party.jpg"},
-  {name: 'Travel',
-  points: 15,
-  img:"http://marketingland.com/wp-content/ml-loads/2016/05/travel-search-suitcasebeach-ss-1920.png"}]
-
 class App extends React.Component {
 
   state = { events, user: [], score: [{ total: 0 }] };
@@ -64,5 +35,34 @@ class App extends React.Component {
     )
   }
 }
+
+const events = [
+  {name:'Swim',
+  points:3,
+  img: "http://media.istockphoto.com/photos/friends-enjoying-a-pool-party-picture-id524323159?k=6&m=524323159&s=612x612&w=0&h=_IBOSTI2_sR_pkaspsp28LyaguoTCZ_-VUhvoGWbLQs="},
+  {name:'Run',
+  points:2,
+  img: "https://s-media-cache-ak0.pinimg.com/originals/9b/95/88/9b958800fc7f853ac84ff29800013c3d.jpg"},
+  {name:'Hike',
+  points:4,
+  img: "http://i52.tinypic.com/m9t9cl.jpg"},
+  {name:'Festival',
+  points:10,
+  img: "http://ampthemag.com/wp-content/uploads/2017/03/fest-girl.jpg"},
+  {name:'BBQ',
+  points:4,
+  img: "http://i4.mirror.co.uk/incoming/article3533182.ece/ALTERNATES/s615/Raging-fire-from-charcoal-barbecue-grill.jpg"},
+  {name: 'Rock Climbing',
+  points: 5,
+  img:"http://www.toptenz.net/wp-content/uploads/2013/05/Rock-Climbing.jpg"},
+  {name: 'Movie Theater',
+  points: 1,
+  img:"http://timothywhite.com/sites/default/files/images/portfolio/pee-wee_herman.jpg"},
+  {name: 'Party',
+  points: 5,
+  img:"http://now-here-this.timeout.com/wp-content/uploads/2013/12/House-party.jpg"},
+  {name: 'Travel',
+  points: 15,
+  img:"http://marketingland.com/wp-content/ml-loads/2016/05/travel-search-suitcasebeach-ss-1920.png"}]
 
 export default App;
